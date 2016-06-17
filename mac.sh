467f8bfa655fdaefe83dfdeedf12ba677f136694
Adrian Thomas-Prestemon <github@adriantp.com>
2016-06-17 15:16:44 -0500

    Added Cask

--- mac.sh
+++ mac.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-# Download Arduino
-# Download Beyond Compare
-# Download Cyberduck
-# Download Flux
-# Download Google Chrome
-# Download Macs Fan Control
-# Download node-webkit
-# Download PhpStorm
-# Download Quassel Client
-# Download Skype
-# Download Slack
-# Download Spectacle
-# Download The Unarchiver
-# Download XAMPP
-
 # Setup VIM Preferences
 echo "
 :set softtabstop=4 shiftwidth=4 expandtab nowrap number
@@ -59,3 +44,28 @@ sudo gem install sass; sudo gem install compass --pre; sudo gem install susy;
 
 # Install Composer
 curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer;
+
+# Install Homebrew Cask
+brew tap caskroom/cask
+
+# Download Arduino
+# Download Beyond Compare
+# Download Cyberduck
+# Download Discord
+# Download Fluid
+# Download Flux
+# Download Google Chrome
+# Download iTerm
+# Download Macs Fan Control
+# Download node-webkit
+# Download PhpStorm
+# Download Quassel Client
+# Download Skype
+# Download Slack
+# Download Spectacle
+# Download sqlitebrowser
+# Download The Unarchiver
+# Download VirtualBox
+# Download XAMPP
+
+brew cask install arduino beyond-compare cyberduck discord fluid flux google-chrome iterm2 macs-fan-control nwjs phpstorm quassel-client skype slack spectacle sqlitebrowser the-unarchiver virtualbox xampp
