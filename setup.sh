4eebd7c49992f1e971edddc116688e6e43a67550
AdrianTP <github@adriantp.com>
2019-02-24 22:28:53 -0600

    bad file paths in root setup script

--- setup.sh
+++ setup.sh
@@ -2,13 +2,13 @@
 
 if [ "$(uname)" == "Darwin" ]; then
   # Mac OS X
-  ./mac.sh
+  ./mac/setup.sh
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
   # Linux
-  ./linux.sh
+  ./linux/setup.sh
 elif [ $var = "/data/data/com.termux/files/usr/bin/bash" ]; then
   # Termux # thanks to https://github.com/KonradIT/dotfiles/blob/master/bashrc#L235
-  ./termux.sh
+  ./termux/setup.sh
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
   # 32-bit Windows
   echo 'Does not support 32-bit Windows yet'
