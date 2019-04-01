0b6bbe5c9d86f071ecc3d76f36130207691ab91c
AdrianTP <github@adriantp.com>
2019-04-01 12:36:16 -0500

    Added a comment/placeholder

--- linux/setup.sh
+++ linux/setup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# Someday I may have packages to install: https://unix.stackexchange.com/a/212214/67744
+
 ln -s ./utils/pdiff.sh /usr/local/bin/pdiff
 ln -s ./utils/serve.sh /usr/local/bin/serve
 ln -s ./utils/stuf.sh /usr/local/bin/stfu
